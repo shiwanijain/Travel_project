0594ef31218d32b73d9a650cec503e5168a0ce48
Pratik Chandra Pal <pratikpal050@gmail.com>
2024-02-28 22:43:54 +0530

    project setup and responsive navbar added

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Navbar from './components/header/Navbar'
+
+function App() {
+
+  return (
+    <div className='overflow-contain'>
+      <Navbar />
+    </div>
+  )
+}
+
+export default App
