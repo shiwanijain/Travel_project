cc5f4289e97f41db0c9820d15a53ce5968f5247d
raghavendrajha119 <raghavendrajha119@gmail.com>
2024-03-05 23:56:54 +0530

    Updated the routes for tours

--- Travel_project/tour/views.py
+++ Travel_project/tour/views.py
@@ -6,38 +6,46 @@ from rest_framework import status
 from .models import Tour
 from .serializers import TourSerializer
 
-@api_view(['GET', 'POST'])
+@api_view(['GET'])
 def all_tours(request):
-    if request.method == 'GET':
-        tours = Tour.objects.all()
-        serializer = TourSerializer(tours, many=True)
-        return Response(serializer.data)
+    tours = Tour.objects.all()
+    serializer = TourSerializer(tours, many=True)
+    return Response(serializer.data)
 
-    elif request.method == 'POST':
-        serializer = TourSerializer(data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(['GET', 'PUT', 'DELETE'])
-def tour_by_id(request, id):
+@api_view(['GET'])
+def get_tour(request, id):
     try:
         tour = Tour.objects.get(id=id)
+        serializer = TourSerializer(tour)
+        return Response(serializer.data)
     except Tour.DoesNotExist:
         return Response(status=status.HTTP_404_NOT_FOUND)
 
-    if request.method == 'GET':
-        serializer = TourSerializer(tour)
-        return Response(serializer.data)
+@api_view(['POST'])
+def create_tour(request):
+    serializer = TourSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
-    elif request.method == 'PUT':
+@api_view(['PUT'])
+def update_tour(request, id):
+    try:
+        tour = Tour.objects.get(id=id)
         serializer = TourSerializer(tour, data=request.data)
         if serializer.is_valid():
             serializer.save()
             return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    except Tour.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
 
-    elif request.method == 'DELETE':
+@api_view(['DELETE'])
+def delete_tour(request, id):
+    try:
+        tour = Tour.objects.get(id=id)
         tour.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
+    except Tour.DoesNotExist:
+        return Response(status=status.HTTP_404_NOT_FOUND)
