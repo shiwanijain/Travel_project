236f25b1567d74793e591bf548b4d40fe7abea8b
raghavendrajha119 <raghavendrajha119@gmail.com>
2024-03-10 21:28:32 +0530

    Backend code update image fields

--- Travel_project/tour/models.py
+++ Travel_project/tour/models.py
@@ -14,7 +14,9 @@ class Tour(models.Model):
     starting_price = models.DecimalField(max_digits=10, decimal_places=2)
     number_of_days = models.PositiveIntegerField()
     contact_details = models.TextField()
-    image = models.ImageField(upload_to='tour_images/', blank=True, null=True)
+    image1 = models.ImageField(upload_to='tour_images/', blank=True, null=True)
+    image2 = models.ImageField(upload_to='tour_images/', blank=True, null=True)
+    image3 = models.ImageField(upload_to='tour_images/', blank=True, null=True)
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
